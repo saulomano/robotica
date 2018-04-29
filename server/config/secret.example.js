814d5e48f8280b8d160c51ab67196115052a9101
root <root@BAQ-014.localdomain>
2018-04-29 13:12:10 -0500

    removed googleId and secretId

--- server/config/secret.example.js
+++ server/config/secret.example.js
@@ -3,13 +3,13 @@ module.exports = function(){
 	if (process.env.NODE_ENV === 'production'){
 		return {
 			SESSION_SECRET: 'topsecretvalue',
-			GOOGLE_ID: '239792274098-ipovv37ljvj9r6rf488g2ujsb9ek45gb.apps.googleusercontent.com',
-			GOOGLE_SECRET: '45GinA8NbrEy_h78pbwIoOso'
+			GOOGLE_ID: '',
+			GOOGLE_SECRET: ''
 		};
 	}
 	return {
 		SESSION_SECRET: 'topsecretvalue',
-		GOOGLE_ID: '239792274098-ipovv37ljvj9r6rf488g2ujsb9ek45gb.apps.googleusercontent.com',
-		GOOGLE_SECRET: '45GinA8NbrEy_h78pbwIoOso'
+		GOOGLE_ID: '',
+		GOOGLE_SECRET: ''
 	};
 }
