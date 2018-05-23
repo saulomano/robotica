3ec14fc67842ae54d0a203a7def39349e3461131
yesica.borina <yesica.borina@gmail.com>
2018-05-23 02:24:38 -0300

    arreglo para nuevo desafio

--- client/social/desafio/desafio.component.js
+++ client/social/desafio/desafio.component.js
@@ -566,8 +566,8 @@ export default class DesafioComponent extends SocialComponent {
     {
         if(this.desafio.type === 'desafio')
         {
-            this.resource.district = (this.selectedDistrict) ? angular.copy(this.selectedDistrict.name) : null;;
-            this.resource.school = (this.selectedSchool) ? angular.copy(this.selectedSchool.schoolName) : null;
+            this.desafio.district = (this.selectedDistrict) ? angular.copy(this.selectedDistrict.name) : null;
+			this.desafio.school = (this.selectedSchool) ? angular.copy(this.selectedSchool.schoolName) : null;
         }
     }
 }
