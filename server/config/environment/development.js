2eba192d5c98d4877ce70d8ba322a895d6355ba2
saulo <saulo.manograsso@gmail.com>
2018-05-23 23:45:25 -0300

    development.js apuntando  a tst

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,10 +7,10 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://127.0.0.1:27017/robotica-dev'
+    uri: 'mongodb://35.232.152.217:27017/robotica-dev'
   },
 
-  // Seed database on startup
+  // Seed database on startupgulp
   seedDB: true
 
 };
