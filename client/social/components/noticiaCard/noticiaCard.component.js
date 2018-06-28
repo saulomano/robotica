6b942c420cd100c0f28824944ae38bf8175c5a9c
saulo <saulo.manograsso@gmail.com>
2018-06-28 01:40:36 -0300

    delete de noticias desde tablero

--- client/social/components/noticiaCard/noticiaCard.component.js
+++ client/social/components/noticiaCard/noticiaCard.component.js
@@ -10,14 +10,14 @@ export default angular
 
 class NoticiaCardController {
 	/*@ngInject*/
-	constructor($scope, $element, $state, $mdDialog){
+	constructor($scope, $element, $state, $mdDialog, $mdToast){
 		this.$scope = $scope;
 		this.$element = $element;
 		this.$state = $state;
         this.$mdDialog = $mdDialog;
 		this.$element.addClass('noticia-card');
-		
-
+        this.deleteUserBoolean = false;
+        this.$mdToast = $mdToast;
 	
 		
 		
@@ -40,7 +40,33 @@ class NoticiaCardController {
 
 	editResource(){
 		this.$state.go(`curador.noticia`, { uid: this.resource._id, action: 'edit' });
-	}
+    }
+  showConfirm(ev) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm =this.$mdDialog.confirm()
+              .title('Eliminar?')
+              .textContent('Esta seguro que desea eliminar la noticia.')
+              .ariaLabel('')
+              .targetEvent(ev)
+              .ok('Si!')
+              .cancel('No');
+              confirm.resource= this.resource;
+        this.$mdDialog.show(confirm).then(function() {
+            confirm.resource
+            .remove()
+            .then( data => {
+                this.$state.go('curador.dashboardnoticias', {}, {reload: true});
+            })
+            .catch( err => {
+                throw err;
+            });
+        }, function() {
+         
+        });
+      };
+
+   
+
 
 	deleteResource(){
         this.resource
