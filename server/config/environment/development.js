2e3bc61fbfd0c166249171fe7123b09727e74bd8
yesica.borina <yesica.borina@gmail.com>
2018-06-29 00:27:12 -0300

    cambios en menu

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,7 +7,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://104.155.187.207:27017/robotica-desarrollo'
+    uri: 'mongodb://35.226.12.143:27017/robotica-desarrollo'
   },
 
   
